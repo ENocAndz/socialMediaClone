df25e98f62865cc092e137eda487cafd08670261
Enoc <21e.andrade7@gmail.com>
2024-03-05 12:38:08 -0600

    register.jsx working with api almost done,  next is login.jsx with api

--- client/socialMediaClone/src/pages/register/Register.jsx
+++ client/socialMediaClone/src/pages/register/Register.jsx
@@ -1,18 +1,45 @@
 import "./register.scss"
 import {Link} from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
+    const [inputs, setInputs] = useState({
+        username:"",
+        email:"",
+        password:"",
+        name:"",
+    });
+    const [err, setErr] = useState(null);
+
+    const handleChange = (e) =>{
+        setInputs(prev=>({...prev, [e.target.name]:e.target.value}));
+    };
+    
+    const handleClick = async (e) =>{
+        e.preventDefault();
+
+        try{
+            await axios.post("http://localhost:8800/api/auth/register", inputs);
+        }catch(err){
+            setErr(err.response.data);
+        }
+    };
+
+    console.log(err);
+
   return (
     <div className='register'>
         <div className="card">
             <div className="left">
                 <h1>Register</h1>
                 <form action="">
-                    <input type="text" placeholder="Username"/>
-                    <input type="email" placeholder="Email" name="" id="" />
-                    <input type="password" placeholder="Password" name="" id="" />
-                    <input type="text" placeholder="Name" />
-                    <button>Register</button>
+                    <input type="text" placeholder="Username" name="username" onChange={handleChange}/>
+                    <input type="email" placeholder="Email" name="email" onChange={handleChange}   />
+                    <input type="password" placeholder="Password" name="password" onChange={handleChange}  />
+                    <input type="text" placeholder="Name" name="name" onChange={handleChange}/>
+                    {err && err}
+                    <button onClick={handleClick}>Register</button>
                 </form>
             </div>
             <div className="right">
